f345c6e6da0c40575100e0e3ca15eb504214fb0d
Hanaasagi <ambiguous404@gmail.com>
2023-07-11 23:35:34 +0900

    chore: target-specific build dependencies

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
-use cc::Build;
-
 fn main() {
-    println!("cargo:rerun-if-changed=src");
     #[cfg(target_os = "windows")]
     {
+        use cc::Build;
         println!("cargo:rustc-link-lib=Kernel32");
         Build::new().file("src/win.cpp").compile("machine-uid");
     }
