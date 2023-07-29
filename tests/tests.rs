162d4de9aeebe0d38671f77a2e9e5e700c2c73c8
Hanaasagi <ambiguous404@gmail.com>
2023-07-29 11:11:30 +0900

    feat(test): add non-empty test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,10 +1,16 @@
-extern crate machine_uid;
+use machine_uid;
 
 #[test]
 fn test_get() {
-	let id = machine_uid::get().unwrap();
-	for _ in 1..100 {
-		let id2 = machine_uid::get().unwrap();
-		assert_eq!(id, id2);
-	}
+    let id = machine_uid::get().unwrap();
+    for _ in 1..100 {
+        let id2 = machine_uid::get().unwrap();
+        assert_eq!(id, id2);
+    }
+}
+
+#[test]
+fn test_not_empty() {
+    let id = machine_uid::get().unwrap();
+    assert_ne!(id, String::new());
 }
